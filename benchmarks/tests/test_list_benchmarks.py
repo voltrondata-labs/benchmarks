a89c8950541224b842c85ae9af3dbdbfb07ca5c7
Diana Clarke <diana.joan.clarke@gmail.com>
2021-03-19 19:09:15 -0600

    Increase BenchmarkList test coverage

--- benchmarks/tests/test_list_benchmarks.py
+++ benchmarks/tests/test_list_benchmarks.py
@@ -1,11 +1,40 @@
 import json
 import os
 
+from .. import _benchmark, _example_benchmarks
 from ..tests._asserts import assert_cli, get_cli_output
 
 
 this_dir = os.path.abspath(os.path.dirname(__file__))
 benchmarks_file = os.path.join(this_dir, "..", "..", "benchmarks.json")
+
+
+def test_list():
+    simple = _example_benchmarks.SimpleBenchmark
+    external = _example_benchmarks.RecordExternalBenchmark
+    cases = _example_benchmarks.CasesBenchmark
+    classes = {
+        "simple": simple,
+        "external": external,
+        "cases": cases,
+        "example": cases,  # should be skipped
+    }
+    actual = _benchmark.BenchmarkList().list(classes)
+    expected = [
+        {
+            "command": "cases ALL --iterations=3 --all=true",
+            "flags": {"language": "Python"},
+        },
+        {
+            "command": "external --iterations=3",
+            "flags": {"language": "Python"},
+        },
+        {
+            "command": "simple --iterations=3",
+            "flags": {"language": "Python"},
+        },
+    ]
+    assert actual == expected
 
 
 def test_list_cli():
