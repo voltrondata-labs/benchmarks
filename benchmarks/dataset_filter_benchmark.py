4e2a72ebb905df7cfe6f533e428f68dd226fbe69
Jonathan Keane <jkeane@gmail.com>
2021-03-25 17:15:27 -0500

    Add R-only partitioned dataset benchmark

--- benchmarks/dataset_filter_benchmark.py
+++ benchmarks/dataset_filter_benchmark.py
@@ -28,3 +28,59 @@ class DatasetFilterBenchmark(_benchmark.Benchmark):
         vendor = pyarrow.dataset.field("vendor_id")
         count = pyarrow.dataset.field("passenger_count")
         return lambda: dataset.to_table(filter=(vendor == "DDS") & (count > 3))
+
+
+
+class PartitionedDatasetFilterBenchmark(_benchmark.Benchmark, _benchmark.BenchmarkR):
+    """
+    Read and filter a partitioned dataset.
+
+        PartitionedDatasetFilterBenchmark().run(<case>, options...)
+
+    Parameters
+    ----------
+    case : str
+        A case name from the benchmark (one of: "vignette", "payment_type_3", "small_no_files")
+    cpu_count : int, optional
+        Set the number of threads to use in parallel operations (arrow).
+    iterations : int, default 1
+        Number of times to run the benchmark.
+    run_id : str, optional
+        Group executions together with a run id.
+
+    Returns
+    -------
+    (result, output) : sequence
+        result : The benchmark result.
+        output : The output from the benchmarked function.
+    """
+
+
+    external, r_only = True, True
+    name, r_name = "dataset-taxi-parquet", "dataset_taxi_parquet"
+    valid_cases = (
+        ["query"],
+        ["vignette"],
+        ["payment_type_3"],
+        ["small_no_files"],
+    )
+    options = {
+        "iterations": {"default": 3, "type": int},
+        "cpu_count": {"type": int},
+    }
+
+    def run(self, case=None, **kwargs):
+        cases = self.get_cases(case, kwargs)
+        for case in cases:
+            tags = {"dataset": "dataset-taxi-parquet", "cpu_count": kwargs.get("cpu_count")}
+            command = self._get_r_command(kwargs, case)
+            yield self.r_benchmark(command, tags, kwargs, case)
+        
+    def _get_r_command(self, options, case):
+        return (
+            f"library(arrowbench); "
+            f"run_one(arrowbench:::{self.r_name}, "
+            f'n_iter={options.get("iterations", 1)}, '
+            f"cpu_count={self.r_cpu_count(options)}, "
+            f'query="{case[0]}")'
+        )
---
+++ benchmarks/tests/test_partitioned_dataset_filter_benchhmark.py
@@ -0,0 +1,53 @@
+import copy
+import json
+
+import pytest
+
+from .. import _sources
+from .. import dataset_filter_benchmark
+from ..tests._asserts import assert_benchmark, assert_context, R_CLI
+
+
+HELP = """
+Usage: conbench dataset-filter [OPTIONS] SOURCE
+
+  Run dataset-taxi-parquet benchmark.
+
+Options:
+  --cpu-count INTEGER
+  --iterations INTEGER   [default: 1]
+  --gc-collect BOOLEAN   [default: true]
+  --gc-disable BOOLEAN   [default: true]
+  --show-result BOOLEAN  [default: true]
+  --show-output BOOLEAN  [default: false]
+  --run-id TEXT          Group executions together with a run id.
+  --help                 Show this message and exit.
+"""
+
+
+def assert_benchmark(result, source, name, case, language="Python"):
+    munged = copy.deepcopy(result)
+    expected = {
+        "name": name,
+        "dataset": source,
+        "cpu_count": None,
+        "gc_collect": True,
+        "gc_disable": True,
+    }
+    if language == "R":
+        del expected["gc_collect"]
+        del expected["gc_disable"]
+        expected["query"] = case[0]
+        expected["language"] = "R"
+    assert munged["tags"] == expected
+    assert_context(munged, language=language)
+
+benchmark = dataset_filter_benchmark.PartitionedDatasetFilterBenchmark()
+@pytest.mark.parametrize("case", benchmark.cases, ids=benchmark.case_ids)
+def test_partitioned_dataset_filter_one(case):
+    pytest.skip("needs a test partitioned dataset")
+    benchmark = dataset_filter_benchmark.PartitionedDatasetFilterBenchmark()
+    [(result, output)] = benchmark.run(case, iterations=1)
+    assert_benchmark(result, "dataset-taxi-parquet", benchmark.name, case, language="R")
+    print(json.dumps(result, indent=4, sort_keys=True))
+    assert R_CLI in str(output)
