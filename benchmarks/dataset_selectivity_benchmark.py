0861323c26715120905990022839c782264b494c
Jianshen Liu <jliu120@ucsc.edu>
2021-05-19 22:37:18 +0000

    Add a benchmark to test dataset filtering performance under different selectivity
    Signed-off-by: Jianshen Liu <jliu120@ucsc.edu>

---
+++ benchmarks/dataset_selectivity_benchmark.py
@@ -0,0 +1,63 @@
+import conbench.runner
+import pyarrow.dataset as ds
+
+from benchmarks import _benchmark
+
+
+@conbench.runner.register_benchmark
+class DatasetSelectivityBenchmark(_benchmark.Benchmark):
+    """Read and filter a dataset with different selectivity."""
+
+    name = "dataset-selectivity"
+    sources = ["nyctaxi_multi_parquet_s3", "chi_traffic_2020_Q1"]
+    sources_test = ["nyctaxi_multi_parquet_s3_sample", "chi_traffic_sample"]
+    valid_cases = (["selectivity"], ["1%"], ["10%"], ["100%"])
+    filters = {
+        "nyctaxi_multi_parquet_s3": {
+            "1%": ds.field("pickup_longitude") < -74.013451,  # 561384
+            "10%": ds.field("pickup_longitude") < -74.002055,  # 5615432
+            "100%": None,  # 56154689
+        },
+        "chi_traffic_2020_Q1": {
+            "1%": ds.field("END_LONGITUDE") < -87.807262,  # 124530
+            "10%": ds.field("END_LONGITUDE") < -87.7624,  # 1307565
+            "100%": None,  # 13038291
+        },
+        "nyctaxi_multi_parquet_s3_sample": {
+            "1%": ds.field("pickup_longitude") < -74.0124,  # 20
+            "10%": ds.field("pickup_longitude") < -74.00172,  # 200
+            "100%": None,  # 2000
+        },
+        "chi_traffic_sample": {
+            "1%": ds.field("END_LONGITUDE") < -87.80726,  # 10
+            "10%": ds.field("END_LONGITUDE") < -87.76148,  # 100
+            "100%": None,  # 1000
+        },
+    }
+
+    def run(self, source, case=None, **kwargs):
+        cases = self.get_cases(case, kwargs)
+        for source in self._get_sources(source):
+            source.download_source_if_not_exists()
+            tags = self.get_tags(kwargs, source)
+            format_str = "parquet"
+            schema = ds.dataset(source.source_paths[0], format=format_str).schema
+            for case in cases:
+                (selectivity,) = case
+                dataset = ds.dataset(
+                    source.source_paths, schema=schema, format=format_str
+                )
+                f = self._get_benchmark_function(dataset, source.name, selectivity)
+                yield self.benchmark(f, tags, kwargs, case)
+
+    def _get_benchmark_function(self, dataset, source, selectivity):
+        return lambda: dataset.to_table(
+            filter=self.filters[source][selectivity]
+        ).num_rows
+
+    def _get_sources(self, source):
+        available_sources = ["ALL", "TEST", *self.sources, *self.sources_test]
+        if source not in available_sources:
+            raise Exception("SOURCE can only be one of {}".format(available_sources))
+
+        return self.get_sources(source)
---
+++ benchmarks/tests/test_dataset_selectivity_benchmark.py
@@ -0,0 +1,76 @@
+import copy
+
+import pytest
+
+from .. import _sources
+from .. import dataset_selectivity_benchmark
+from ..tests._asserts import assert_cli, assert_context
+
+
+HELP = """
+Usage: conbench dataset-selectivity [OPTIONS] SOURCE
+
+  Run dataset-selectivity benchmark(s).
+
+  For each benchmark option, the first option value is the default.
+
+  Valid benchmark combinations:
+  --selectivity=1%
+  --selectivity=10%
+  --selectivity=100%
+
+  To run all combinations:
+  $ conbench dataset-selectivity --all=true
+
+Options:
+  --selectivity [1%|10%|100%]
+  --all BOOLEAN                [default: False]
+  --cpu-count INTEGER
+  --iterations INTEGER         [default: 1]
+  --drop-caches BOOLEAN        [default: False]
+  --gc-collect BOOLEAN         [default: True]
+  --gc-disable BOOLEAN         [default: True]
+  --show-result BOOLEAN        [default: True]
+  --show-output BOOLEAN        [default: False]
+  --run-id TEXT                Group executions together with a run id.
+  --run-name TEXT              Name of run (commit, pull request, etc).
+  --help                       Show this message and exit.
+"""
+
+
+nyctaxi = _sources.Source("nyctaxi_multi_parquet_s3_sample")
+chi_traffic = _sources.Source("chi_traffic_sample")
+benchmark = dataset_selectivity_benchmark.DatasetSelectivityBenchmark()
+cases, case_ids = benchmark.cases, benchmark.case_ids
+
+
+def assert_benchmark(result, case, source):
+    munged = copy.deepcopy(result)
+
+    expected = {
+        "cpu_count": None,
+        "dataset": source,
+        "name": "dataset-selectivity",
+        "selectivity": case[0],
+    }
+    assert munged["tags"] == expected
+    assert_context(munged)
+
+
+def assert_run(run, index, case, source):
+    result, output = run[index]
+    assert_benchmark(result, case, source.name)
+    assert output > 0
+
+
+@pytest.mark.parametrize("case", cases, ids=case_ids)
+def test_dataset_selectivity(case):
+    run = list(benchmark.run("TEST", case, iterations=1))
+    assert len(run) == 2
+    assert_run(run, 0, case, nyctaxi)
+    assert_run(run, 1, case, chi_traffic)
+
+
+def test_dataset_selectivity_cli():
+    command = ["conbench", "dataset-selectivity", "--help"]
+    assert_cli(command, HELP)
