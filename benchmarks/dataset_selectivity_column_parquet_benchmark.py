00a4069717e4277f0e3624aec4b2f967fc9f69c7
Rahul Agrawal <41531498+rahul799@users.noreply.github.com>
2021-07-31 01:14:20 +0530

    Update dataset_selectivity_column_parquet_benchmark.py

--- benchmarks/dataset_selectivity_column_parquet_benchmark.py
+++ benchmarks/dataset_selectivity_column_parquet_benchmark.py
@@ -12,36 +12,33 @@ class DatasetColumnSelectivityParquetBenchmark(_benchmark.Benchmark):
     arguments = ["source"]
     sources = ["nyctaxi_2010-01"]
     sources_test = ["nyctaxi_sample"]
+    valid_cases = (["selectivity"], ["1"], ["2"], ["5"], ["10"], ["14"])
 
     def run(self, source, **kwargs):
         for source in self.get_sources(source):
+            tags = self.get_tags(kwargs, source)
             #path = source.create_if_not_exists("parquet", "lz4")
             path = '/mnt/cephfs/nyc'
             dataset = pyarrow.dataset.dataset(path, format="parquet")
-            # array of number of columns
-            col_selectivity = [1, 2, 5, 10, 14]
-            f = self._get_benchmark_function( dataset, col_selectivity)
-            tags = self.get_tags(kwargs, source)
-            yield self.benchmark(f, tags, kwargs)
+            for case in cases:
+                (selectivity,) = case
+                f = self._get_benchmark_function( dataset, selectivity)
+                yield self.benchmark(f, tags, kwargs)
 
-    def _get_benchmark_function(self, dataset, col_selectivity):
-        #columns = ['tpep_pickup_datetime', 'tpep_dropoff_datetime', 'passenger_count', 'trip_distance', 'RatecodeID', 'store_and_fwd_flag', 'PULocationID', 'DOLocationID', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount', 'improvement_surcharge', 'total_amount']
+    def _get_benchmark_function(self, dataset, selectivity):
         columns = dataset.to_table().to_pandas().columns.values.tolist()
         
         # Default value for columns_
         columns_ = columns[:1]
-        for per in col_selectivity:
-            if per == "1":
-                columns_ = columns[:1]
-            if per == "2":
-                columns_ = columns[:2]
-            if per == "5":
-                columns_ = columns[:5]
-            if per == "10":
-                columns_ = columns[:10]
-            if per == "14":
-                columns_ = columns[:14]
-            
-            dataset.to_table(columns=columns_)
+        if selectivity == "1":
+            columns_ = columns[:1]
+        elif selectivity == "2":
+            columns_ = columns[:2]
+        elif selectivity == "5":
+            columns_ = columns[:5]
+        elif selectivity == "10":
+            columns_ = columns[:10]
+        elif selectivity == "14":
+            columns_ = columns[:14]
 
         return lambda: dataset.to_table(columns=columns_)
