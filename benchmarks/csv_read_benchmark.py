785332d19291e93700cc4cca9ca4d172570e81de
Diana Clarke <diana.joan.clarke@gmail.com>
2021-08-12 12:12:34 -0600

    Still segfaults for me

--- benchmarks/csv_read_benchmark.py
+++ benchmarks/csv_read_benchmark.py
@@ -23,24 +23,29 @@ class CsvReadBenchmark(_benchmark.Benchmark):
         cases = self.get_cases(case, kwargs)
         for source in self.get_sources(source):
             for case in cases:
-                (streaming, compression) = case
-                schema = source.table.schema
-                path = source.create_if_not_exists("csv", compression)
-                munged = compression if compression != "uncompressed" else None
-                f = self._get_benchmark_function(streaming, path, munged, schema)
+                f = self._get_benchmark_function(source, *case)
                 tags = self.get_tags(kwargs, source)
                 yield self.benchmark(f, tags, kwargs, case)
 
-    def _get_benchmark_function(self, streaming, path, munged, schema):
-        def read_csv_streaming():
-            return pyarrow.csv.open_csv(
+    def _get_benchmark_function(self, source, streaming, compression):
+        # Note: this will write a comma separated csv with a header, even if
+        # the original source file lacked a header and was pipe delimited.
+        path = source.create_if_not_exists("csv", compression)
+        munged = compression if compression != "uncompressed" else None
+        schema = source.table.schema
+
+        def read_streaming():
+            table = pyarrow.csv.open_csv(
                 pyarrow.input_stream(path, compression=munged),
                 convert_options=pyarrow.csv.ConvertOptions(column_types=schema),
             ).read_all()
+            return table
 
-        def read_csv_file():
-            return pyarrow.csv.read_csv(
+        def read_file():
+            table = pyarrow.csv.read_csv(
                 pyarrow.input_stream(path, compression=munged),
+                convert_options=pyarrow.csv.ConvertOptions(column_types=schema),
             )
+            return table
 
-        return read_csv_streaming if streaming == "streaming" else read_csv_file
+        return read_streaming if streaming == "streaming" else read_file
