83d9a0742a6345d7557f38243c98039a74321307
Elena Henderson <elena@e.local>
2021-10-05 16:28:07 -0700

    Revert "R TPC-H 1 to 10 (#86)"
    This reverts commit a8b4ddb60f02a1099497906cbded4009cbbe3c77.

--- benchmarks/tpch_benchmark.py
+++ benchmarks/tpch_benchmark.py
@@ -5,7 +5,7 @@ from benchmarks import _benchmark
 
 def get_valid_cases():
     result = [["query_id", "scale_factor", "format"]]
-    for query_id in range(1, 11):
+    for query_id in [1, 6]:
         for scale_factor in [1, 10]:
             for _format in ["native", "parquet", "feather"]:
                 result.append([query_id, scale_factor, _format])
