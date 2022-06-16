7d78c9d2a4dcbfeb7567413ba66dbfaa8371260c
Edward Visel <1693477+alistaire47@users.noreply.github.com>
2022-06-16 13:43:44 -0500

    Hotfix: Renamed query in R dataset benchmark

--- benchmarks/tests/test_partitioned_dataset_filter_benchmark.py
+++ benchmarks/tests/test_partitioned_dataset_filter_benchmark.py
@@ -16,13 +16,13 @@ Usage: conbench partitioned-dataset-filter [OPTIONS]
   --query=vignette
   --query=payment_type_3
   --query=small_no_files
-  --query=count_rows
+  --query=dims
 
   To run all combinations:
   $ conbench partitioned-dataset-filter --all=true
 
 Options:
-  --query [count_rows|payment_type_3|small_no_files|vignette]
+  --query [dims|payment_type_3|small_no_files|vignette]
   --all BOOLEAN                   [default: false]
   --iterations INTEGER            [default: 1]
   --drop-caches BOOLEAN           [default: false]
