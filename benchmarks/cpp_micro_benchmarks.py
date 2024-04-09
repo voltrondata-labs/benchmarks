807b920ef56d3b9141420e8f5d49079b10e69afc
Austin Dickey <austin3spammy@gmail.com>
2024-04-09 14:30:27 -0500

    bump up reps again

--- benchmarks/cpp_micro_benchmarks.py
+++ benchmarks/cpp_micro_benchmarks.py
@@ -13,7 +13,7 @@ log.setLevel(logging.DEBUG)
 
 OPTIONS = {
     "repetitions": {
-        "default": 1,
+        "default": 4,
         "type": int,
         "help": "Number of repetitions to tell the executable to run.",
     },
